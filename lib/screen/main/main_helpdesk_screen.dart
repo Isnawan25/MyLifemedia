061d012e6c1f84dd775105acee71b2eec1e03537
Isnawan25 <isnawanrifai@gmail.com>
2026-01-06 00:57:03 +0700

    memperbaiki dan merapikan kode

--- lib/screen/main/main_helpdesk_screen.dart
+++ lib/screen/main/main_helpdesk_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mylm/base/widgets/bottom_nav.dart';
+import 'package:mylm/data/cubit/current_cust_loaded/current_cust_cubit.dart';
+import 'package:mylm/data/cubit/exists_package/exists_package_cubit.dart';
+import 'package:mylm/data/cubit/notification/notification_read_cubit.dart';
+import 'package:mylm/data/cubit/profile/profile_cubit.dart';
 import 'package:mylm/data/cubit/promotions/promotions_cubit.dart';
+import 'package:mylm/data/network/services/get/get_exists_package.dart';
+import 'package:mylm/data/network/services/get/get_profile.dart';
 import 'package:mylm/data/network/services/get/get_promotions.dart';
 import 'package:mylm/screen/main/faq/faq_screen.dart';
 import 'package:mylm/screen/main/helpdesk/helpdesk_screen.dart';
 import 'package:mylm/screen/main/home/home_screen.dart';
 import 'package:mylm/screen/main/riwayat_tagihan/riwayat_tagihan_screen.dart';
 import 'package:mylm/screen/main/user_profile/profile_screen.dart';
+import 'package:mylm/data/cubit/current_cust_loaded/current_cust_state.dart';
+
 
 class MainHelpdeskScreen extends StatefulWidget {
   final String custNumber;
@@ -16,8 +24,8 @@ class MainHelpdeskScreen extends StatefulWidget {
 
   const MainHelpdeskScreen({
     super.key,
-    required this.accessToken,
     required this.custNumber,
+    required this.accessToken,
     required this.custGroupId,
   });
 
@@ -27,59 +35,103 @@ class MainHelpdeskScreen extends StatefulWidget {
 
 class _MainHelpdeskScreenState extends State<MainHelpdeskScreen> {
   int _currentIndex = 4;
+
+  late String _custNumber;
+  late String _custGroupId;
+
   late List<Widget> _screens;
 
   @override
   void initState() {
     super.initState();
 
+    _custNumber = widget.custNumber;
+    _custGroupId = widget.custGroupId;
+
+    _buildScreens();
+  }
+
+  void _buildScreens() {
     _screens = [
       ProfileScreen(
-        custNumber: widget.custNumber,
+        custNumber: _custNumber,
         accessToken: widget.accessToken,
-        custGroupId: widget.custGroupId,
+        custGroupId: _custGroupId,
       ),
       RiwayatTagihanScreen(
-        custNumber: widget.custNumber,
+        custNumber: _custNumber,
         accessToken: widget.accessToken,
-        custGroupId: widget.custGroupId,
+        custGroupId: _custGroupId,
       ),
-      BlocProvider(
-        create: (_) => PromotionsCubit(PromotionService())
-          ..fetchPromotions(),
-        child: HomeScreen(
-          custNumber: widget.custNumber,
-          accessToken: widget.accessToken,
-          custGroupId: widget.custGroupId,
-        ),
+      HomeScreen(
+        custNumber: _custNumber,
+        accessToken: widget.accessToken,
+        custGroupId: _custGroupId,
       ),
       FaqScreen(
-        custNumber: widget.custNumber,
+        custNumber: _custNumber,
         accessToken: widget.accessToken,
-        custGroupId: widget.custGroupId,
+        custGroupId: _custGroupId,
       ),
       HelpdeskScreen(
-        custNumber: widget.custNumber,
+        custNumber: _custNumber,
         accessToken: widget.accessToken,
-        custGroupId: widget.custGroupId,
+        custGroupId: _custGroupId,
       ),
     ];
   }
 
   void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
+    setState(() => _currentIndex = index);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNav(
-        currentIndex: _currentIndex,
-        onTap: _onTap,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => CurrentCustCubit()
+            ..load(
+              defaultCustNumber: _custNumber,
+              defaultCustGroupId: _custGroupId,
+            ),
+        ),
+        BlocProvider(
+          create: (_) => ProfileCubit(ProfileService()),
+        ),
+        BlocProvider(
+          create: (_) => ExistsPackageCubit(ExistsPackageService()),
+        ),
+        BlocProvider(
+          create: (_) => NotificationReadCubit(),
+        ),
+        BlocProvider(
+          create: (_) => PromotionsCubit(PromotionService())
+            ..fetchPromotions(),
+        ),
+      ],
+      child: BlocListener<CurrentCustCubit, CurrentCustState>(
+        listener: (context, state) {
+          if (state is CurrentCustLoaded) {
+
+            setState(() {
+              _custNumber = state.custNumber;
+              _custGroupId = state.custGroupId;
+              _buildScreens(); // rebuild SEMUA tab
+            });
+          }
+        },
+        child: Scaffold(
+          body: _screens[_currentIndex],
+          bottomNavigationBar: BottomNav(
+            currentIndex: _currentIndex,
+            onTap: _onTap,
+          ),
+        ),
       ),
     );
   }
 }
+
+
+
